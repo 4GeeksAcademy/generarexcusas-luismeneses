08cde8f28300777454d6133741316f1e36adee5b
Luismeneses176 <luismeneses176@gmail.com>
2025-03-15 19:22:41 +0000

    proyecto final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  const createRandomNumber = (limit) =>{
+    return Math.floor(Math.random() * limit);
+
+  }
+  const randomWho = who[createRandomNumber(who.length)]
+  const randoAction = action[createRandomNumber(action.length)]
+  const randomWhat = what[createRandomNumber(what.length)]
+  const randomWhen = when[createRandomNumber(when.length)]
+
+  const excuse = randomWho+" "+randoAction+" "+randomWhat+" "+randomWhen
+  const element = document.getElementById("myExcuse")
+  element.innerHTML= excuse
+  console.log(excuse);
 };
