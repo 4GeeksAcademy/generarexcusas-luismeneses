5fe5d2127df65bb73190dd04d677447c1346cfbc
Luismeneses176 <luismeneses176@gmail.com>
2025-03-17 20:06:35 +0000

    excelente

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
+window.onload = function () {
+
   const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   const action = ['ate', 'peed', 'crushed', 'broke'];
   const what = ['my homework', 'my phone', 'the car'];
   const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  const createRandomNumber = (limit) =>{
+  const createRandomNumber = (limit) => {
     return Math.floor(Math.random() * limit);
 
   }
@@ -21,7 +21,7 @@ window.onload = function() {
   const randomWhat = what[createRandomNumber(what.length)]
   const randomWhen = when[createRandomNumber(when.length)]
 
-  const excuse = randomWho+" "+randoAction+" "+randomWhat+" "+randomWhen
+  const excuse = randomWho + " " + randoAction + " " + randomWhat + " " + randomWhen
   const element = document.getElementById("myExcuse")
-  element.innerHTML= excuse
+  element.innerHTML = excuse
 };
